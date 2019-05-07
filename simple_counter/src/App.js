87cc4c9ccf67be99c22a55ddd68587f0dd417e05
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-05-07 11:37:27 -0700

    updatedreadme

--- simple_counter/src/App.js
+++ simple_counter/src/App.js
@@ -9,30 +9,36 @@ const Header = () => {
   );
 };
 
-class Content extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
     count: 0,
+    totalClick:0,
     };
   }
 
   handleCountAdd = e => {
     e.preventDefault();
     let count = this.state.count +1;
+    let totalClick = this.state.totalClick+1;
     this.setState({ count });
+    this.setState({ totalClick });
   };
 
   handleCountMin = e => {
     e.preventDefault();
     let count = this.state.count -1;
+    let totalClick = this.state.totalClick +1;
     this.setState({ count });
+    this.setState({ totalClick });
   };
 
   render() {
     return (
       <div>
+        <p>Total Number of Clicks {this.state.totalClick}</p>
         <p>{this.state.count}</p>
         <button onClick={this.handleCountMin}>-</button>
         <button onClick={this.handleCountAdd}>+</button>
@@ -46,7 +52,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Header />
-        <Content />
+        <Counter />
       </React.Fragment>
     );
   }
